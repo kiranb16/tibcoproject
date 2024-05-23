5a293a57ac2e5a124157a78f9ee6f91689d56b13
kiranb16 <kiranbatwal996@gmail.com>
2024-05-23 22:00:10 +0530

    updaed Employee

--- TibcoPojectDevelopment/src/Employee.java
+++ TibcoPojectDevelopment/src/Employee.java
@@ -5,6 +5,31 @@ public class Employee {
  private String name;
  
  private String state;
+
+public int getId() {
+	return id;
+}
+
+public void setId(int id) {
+	this.id = id;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getState() {
+	return state;
+}
+
+public void setState(String state) {
+	this.state = state;
+}
+ 
  
  
 }
